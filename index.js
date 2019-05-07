96610d7bb3b03e13591c6f41681034d6f08929d5
Walker <walker@praiseweb.com.br>
2019-05-06 23:05:07 -0300

    add calulation

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-// console.log('alavancagem para obter', lucro, '% de lucro');
-// calculaAcoes(valorOperacao, taxaImposto, lucro, stopLoss, stopGain1, stopGain2, stopGain3)
+function imprimeStops(args) {
+    const { _, l, tx, op } = args;
+    const [cmd, t, sl, sg1, sg2, sg3] = _;
+    let q = 3;
+    let cost = 0;
+    let profit = 0;
+    do {
+        const qq = q / 3;
+        const taxes = op + op * (1 + tx / 100) * 2;
+        const buy = t * q;
+        const sell = sg1 * qq + sg2 * qq + sg3 * qq;
+        cost = buy + taxes
+        profit = sell - cost;
+        const minProfit = buy * l / 100;
+        done = profit >= minProfit;
+        q += 3;
+    } while (!done && q < 10000);
+    if (!done) {
+        console.log('lucro de', l, '% não realisável');
+        if (l > 1) {
+            const newL = Math.floor(l * .9);
+            console.log('tentando com lucro de', newL, '%');
+            imprimeStops({...args, l: newL});
+        }
+    } else {
+        console.log('lucro de', l, '% com a seguinte configuração');
+        console.log('trigger', t);
+        console.log('stop loss', sl);
+        console.log('stop gain em', sg1, 'com', q/3, 'ações');
+        console.log('stop gain em', sg2, 'com', q/3, 'ações');
+        console.log('stop gain em', sg3, 'com', q/3, 'ações');
+        console.log('custo da operação', Math.floor(cost));
+        console.log('lucro da operação', Math.floor(profit));
+    }
+}
 
 const argv = require('yargs')
     .usage('Uso: $0 <comando> [opcoes]')
@@ -21,3 +54,4 @@ const argv = require('yargs')
     .help('h')
     .argv;
 
+imprimeStops(argv)
